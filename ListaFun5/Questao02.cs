bfa1adcd9b0f1b020e68e85a463510ed7fad51ab
cristian-henrique <cristianmsbr@gmail.com>
2016-05-28 08:38:05 -0300

    Segunda lista de exercícios

---
+++ ListaFun5/Questao02.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Questao02 {
+	public static void Main (string[] args) {
+		Console.Write("Digite um número inteiro: ");
+		int n = int.Parse(Console.ReadKey());
+
+		if (b % 5 == 0) Console.WriteLine("O número é múltiplo de 5");
+		else Console.WriteLine("O número não é múltiplo de 5");
+	}
+}
