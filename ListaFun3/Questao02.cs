4d16c61cade9be6e7ca2222f9e4a9c342dd2e86c
cristian-henrique <cristianmsbr@gmail.com>
2016-05-29 13:01:53 -0300

    Segunda lista de exercícios

---
+++ ListaFun3/Questao02.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Questao02 {
+	public static void Main (string[] args) {
+		for (int i = 200; i >= 1; i--) {
+			Console.WriteLine(i);
+		}
+
+		Console.ReadKey();
+	}
+}
