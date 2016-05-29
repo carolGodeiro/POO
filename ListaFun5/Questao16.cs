1a759ab5ae016d204dc924785388c551c7ef4165
cristian-henrique <cristianmsbr@gmail.com>
2016-05-28 21:17:34 -0300

    Exercícios da segunda lista

--- ListaFun5/Questao16.cs
+++ ListaFun5/Questao16.cs
@@ -2,6 +2,14 @@ using System;
 
 public class Questao16 {
 	public static void Main (string[] args) {
-		
+		Console.Write("Ângulo: ");
+		double a = double.Parse(Console.ReadLine());
+
+		if (a > 0 && a < 90) Console.WriteLine("Primeiro quadrante");
+		else if (a > 90 && a < 180) Console.WriteLine("Segundo quadrante");
+		else if (a > 180 && a < 270) Console.WriteLine("Terceiro quadrante");
+		else if (a > 270 && a < 360) Console.WriteLine("Quarto quadrante");
+
+		Console.WriteLine("O ângulo dá " + (a / 360) + " voltas");
 	}
 }
