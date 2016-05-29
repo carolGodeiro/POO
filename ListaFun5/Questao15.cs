9f3a49eae57992fece01071851208872ce624689
cristian-henrique <cristianmsbr@gmail.com>
2016-05-28 21:17:21 -0300

    Exercícios da segunda lista

--- ListaFun5/Questao15.cs
+++ ListaFun5/Questao15.cs
@@ -5,9 +5,9 @@ public class Questao15 {
 		Console.Write("Ângulo: ");
 		double a = double.Parse(Console.ReadLine());
 
-		if (a >= 0 && a <= 90) Console.WriteLine("Primeiro quadrante");
-		else if (a > 90 && a <= 100) Console.WriteLine("Segundo quadrante");
-		else if (a > 180 && a <= 270) Console.WriteLine("Terceiro quadrante");
-		else Console.WriteLine("Quarto quadrante");
+		if (a > 0 && a < 90) Console.WriteLine("Primeiro quadrante");
+		else if (a > 90 && a < 180) Console.WriteLine("Segundo quadrante");
+		else if (a > 180 && a < 270) Console.WriteLine("Terceiro quadrante");
+		else if (a > 270 && a < 360) Console.WriteLine("Quarto quadrante");
 	}
 }
