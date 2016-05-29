c2bb1abdf0415230e2b80b0bac5565938dc2bcf3
cristian-henrique <cristianmsbr@gmail.com>
2016-05-29 13:02:02 -0300

    Segunda lista de exercícios

---
+++ ListaFun3/Questao03.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Questao03 {
+	public static void Main (string[] args) {
+		int soma = 0;
+
+		for (int i = 0; i <= 200; i++) {
+			soma += i;
+			i++;
+		}
+
+		Console.WriteLine(soma);
+		Console.ReadKey();
+	}
+}
