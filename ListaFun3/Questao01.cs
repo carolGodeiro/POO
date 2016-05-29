f7ab69d18ce30bf45af75a143535b19afd3fde2e
cristian-henrique <cristianmsbr@gmail.com>
2016-05-29 13:00:52 -0300

    Terceira lista de exercícios

---
+++ ListaFun3/Questao01.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Questao01 {
+	public static void Main(string[] args) {
+		for (int i = 1; i <= 100; i++) {
+			Console.WriteLine(i);
+		}
+
+		Console.ReadKey();
+	}
+}
