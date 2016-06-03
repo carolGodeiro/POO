361e96564c0fe390464cc25bc93215ba9497cd29
cristian-henrique <cristianmsbr@gmail.com>
2016-06-03 09:11:03 -0300

    Terceira lista de exercícios

---
+++ ListaFun3/Questao23.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Questao23 {
+	public static void Main (string[] args) {
+		Console.Write("N: ");
+		int n = int.Parse(Console.ReadLine());
+		int c = 0;
+
+		if (n > 0) {
+			for (int i = 1; i <= n; i++) {
+				if (n % i == 0) c++;
+			}
+
+			if (c <= 2) Console.WriteLine("Primo");
+			else Console.WriteLine("Não é primo");
+		}
+	}
+}
