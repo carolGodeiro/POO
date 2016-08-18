a23219073b3a0287c823ed1ae1470a1434e16d77
cristian-henrique <cristianmsbr@gmail.com>
2016-08-18 07:00:25 -0300

    Lista sobre funções e procedimentos

---
+++ ListaFuncoes/Questao10.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Questao08 {
+	public static void Main (string[] args) {
+		Console.WriteLine(kelvin(27));
+	}
+
+	private static double kelvin (double c) {
+		return c + 273;
+	}
+}
