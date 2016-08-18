81a9f250b4bf95c4faa78ba8bd073291c1535278
cristian-henrique <cristianmsbr@gmail.com>
2016-08-18 07:04:48 -0300

    Lista sobre funções e procedimentos

--- ListaFuncoes/Questao05.cs
+++ ListaFuncoes/Questao05.cs
@@ -2,6 +2,10 @@ using System;
 
 public class Questao05 {
 	public static void Main (string[] args) {
-		
+		Console.WriteLine(raizC(125));
+	}
+
+	private static double raizC (int n) {
+		return Math.Round(Math.Pow(n, 0.333333333));
 	}
 }
