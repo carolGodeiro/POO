2a002b10b30b2be90d74df481b255fd04bf5f39c
cristian-henrique <cristianmsbr@gmail.com>
2016-08-19 08:41:49 -0300

    Lista sobre matrizes

--- ListaArrays/Questao17.cs
+++ ListaArrays/Questao17.cs
@@ -1,7 +1,27 @@
 ﻿using System;
 
-public class Questao15 {
-    public static void Main (string[] args) {
-        
-    }
+public class Questao17 {
+	public static void Main (string[] args) {
+		int[] a = new int[30];
+		string aux = "";
+		string maior = "";
+
+		for (int i = 0; i < a.Length; i++) {
+			Console.Write("N" + (i + 1) + ": ");
+			a[i] = int.Parse(Console.ReadLine());
+		}
+
+		for (int i = 1; i < a.Length; i++) {
+			if (a[i] > a[i - 1]) {
+				aux += "a";
+				if (aux.Length > maior.Length) {
+					maior = aux;
+				}
+			} else {
+				aux = "";
+			}
+		}
+
+		Console.WriteLine("A maior sublista ordenada crescente tem tamanho " + (maior.Length + 1));
+	}
 }
